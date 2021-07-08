47c73ad832d72ec21d356c4ef818aaafe6249f0f
Reece Harris <reece.harris98@protonmail.com>
2021-07-08 21:34:05 +0100

    Delete AutoWindowsInstall.bat

--- AutoWindowsInstall.bat
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/NotReeceHarris/DataStore3.git
-cd DataStore3/server
-pip install -r requirements.txt --no-warn-script-location
-cls
-python CreateUser.py
-python main.py
