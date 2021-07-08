a0ccffb5bc8c38cf66eee93785690ef1ab3777dc
Reece Harris <reece.harris98@protonmail.com>
2021-07-08 13:35:17 +0100

    Update DataStore3Example.lua

--- roblox/DataStore3Example.lua
+++ roblox/DataStore3Example.lua
@@ -1,4 +1,20 @@
-local DataStore3 = require(game:GetService("ServerScriptService").DataStore3Libary);
+-- $$$$$$$\             $$\                $$$$$$\    $$\                                          $$$$$$\  
+-- $$  __$$\            $$ |              $$  __$$\   $$ |                                        $$ ___$$\ 
+-- $$ |  $$ | $$$$$$\ $$$$$$\    $$$$$$\  $$ /  \__|$$$$$$\    $$$$$$\   $$$$$$\   $$$$$$\        \_/   $$ |
+-- $$ |  $$ | \____$$\\_$$  _|   \____$$\ \$$$$$$\  \_$$  _|  $$  __$$\ $$  __$$\ $$  __$$\         $$$$$ / 
+-- $$ |  $$ | $$$$$$$ | $$ |     $$$$$$$ | \____$$\   $$ |    $$ /  $$ |$$ |  \__|$$$$$$$$ |        \___$$\ 
+-- $$ |  $$ |$$  __$$ | $$ |$$\ $$  __$$ |$$\   $$ |  $$ |$$\ $$ |  $$ |$$ |      $$   ____|      $$\   $$ |
+-- $$$$$$$  |\$$$$$$$ | \$$$$  |\$$$$$$$ |\$$$$$$  |  \$$$$  |\$$$$$$  |$$ |      \$$$$$$$\       \$$$$$$  |
+-- \_______/  \_______|  \____/  \_______| \______/    \____/  \______/ \__|       \_______|       \______/ 
+
+-- Create by Reece Harris (https://github.com/NotReeceHarris) & Deven Briers (https://github.com/NotDevenBriers)
+-- This service is a SQL datastore for roblox, allowing multiple users to create multiple databases.
+-- $Apache2 License.
+-- https://github.com/NotReeceHarris/DataStore3
+
+-- This file is an example, any values such as gold, wood and gems can be changed do what ever you want your are not restricted by value amounts ever so you can stop up too 5 Gb of data (Not per person)
+
+local DataStore3 = require(game:GetService("ServerScriptService").DataStore3Libary);  --Imports the DataStore3 libary
 
 -- Database hostname eg. (IPV4, URL) [DO NOT INCLUDE HTTP:// OR WWW.]
 local url = ""
