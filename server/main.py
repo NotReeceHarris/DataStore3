165d8420281aec7f27cfee98da79c681f721de06
Reece Harris <reece.harris98@protonmail.com>
2021-07-08 01:19:47 +0100

    Update main.py

--- server/main.py
+++ server/main.py
@@ -12,9 +12,14 @@ Create by Reece Harris (https://github.com/NotReeceHarris) & Deven Briers (https
 This service is a SQL datastore for roblox, allowing multiple users to create multiple databases.
 - Apache2 License.
 https://github.com/NotReeceHarris/DataStore3
+
+
+
+This code is open source, all backend SQL executions will stay up to date
 '''
 
 from flask import Flask, redirect, url_for, render_template, request, session, flash, send_file, jsonify
+from flask_assets import Environment, Bundle
 import sqlite3
 import hashlib
 import json
@@ -22,10 +27,16 @@ import datetime
 import random
 import string
 import os
+from waitress import serve
 
 app = Flask(__name__)
+assets = Environment(app)
 app.permanent_session_lifetime = datetime.timedelta(days=365)
 app.secret_key = ''.join(random.choice(string.ascii_letters) for i in range(100)).encode('ascii')
+
+from sqlExecuter import SqlExecutionApi
+app.register_blueprint(SqlExecutionApi)
+
 
 @app.route('/')
 def index():
@@ -323,114 +334,12 @@ def importPost():
     else:
         return redirect(url_for("Four0Four"))
 
-#------------------------------------------------------------------------------------------- REMOTE SQL EXECUTER
 
-def Ipfilter(ip):
-    if ":" in ip:
-        if json.load(open("config.json"))["ipfilter"]:
-            return ip in json.load(open("ipRange.json"))
-        else:
-            return True
-    else:
-        if json.load(open("config.json"))["ipfilter"]:
-            return '.'.join([str(elem) for elem in ip.split(".")[:-1]]) in json.load(open("ipRange.json"))
-        else:
-            return True
-
-@app.route('/api/payload/post/', methods=["POST"])
-def apiPayloadPost():
-
-    if not Ipfilter(request.remote_addr):
-        return jsonify({"ReturnCode": 0, "ErrorCode":"You are not roblox"}), 401
-
-    Key = request.form["key"]
-    Username = request.form["username"]
-    Payload = request.form["payload"]
-    conn = sqlite3.connect(f'SqliteStorage/apiKeys.db')
-    c = conn.cursor()
-    c.execute(f"SELECT * FROM Keys WHERE _key='{Key}'")
-    apikey = c.fetchall()
-    conn.close()
-    dbfile = ""
-    if apikey == []:
-        print("hi2")
-        return jsonify({"ReturnCode": 0, "ErrorCode":"Invalid Api Key"}), 401
-    else:
-        if Key == None or Payload == None:
-            print("hi4")
-            return jsonify({"ReturnCode": 0, "ErrorCode":"Missing Attributes"}), 505
-        else:
-            path = f"SqliteStorage\\{Username}"
-            for root, dirs, files in os.walk(path):
-                for filename in files:
-                    if filename.startswith(apikey[0]):
-                        dbfile = filename
-            if dbfile == None:
-                return jsonify({"ReturnCode": 0, "ErrorCode":"Missing database files, message a system admin"}), 505
-            conn = sqlite3.connect(f'SqliteStorage/{Username}/{dbfile}')
-            c = conn.cursor()
-            c.execute("SELECT name FROM sqlite_master WHERE type='table';")
-            response = c.fetchall()
-            try:
-                response = c.execute(Payload)
-                conn.commit()
-                conn.close()
-                return jsonify({"ReturnCode": 1}), 200
-            except sqlite3.OperationalError as a:
-                return jsonify({"ReturnCode": 0, "ErrorCode": str(a)}), 200
-
-
-@app.route('/api/payload/get/', methods=["POST"])
-def apiPayloadGet():
-    if not Ipfilter(request.remote_addr):
-        return jsonify({"ReturnCode": 0, "ErrorCode":"You are not roblox"}), 401
-
-    Key = request.form["key"]
-    Username = request.form["username"]
-    Payload = request.form["payload"]
-
-    conn = sqlite3.connect(f'SqliteStorage/apiKeys.db')
-    c = conn.cursor()
-    c.execute(f"SELECT * FROM Keys WHERE _key='{Key}'")
-
-    apikey = c.fetchall()
-    conn.close()
-
-    dbfile = ""
-
-    if apikey == []:
-        return jsonify({"ReturnCode": 0, "ErrorCode":"Invalid Api Key"}), 401
-    else:
-
-        if Key == None or Payload == None:
-            return jsonify({"ReturnCode": 0, "ErrorCode":"Missing Attributes"}), 505
-        else:
-            path = f"SqliteStorage\\{Username}"
-            for root, dirs, files in os.walk(path):
-                for filename in files:
-                     if filename.startswith(apikey[0]):
-                        dbfile = filename
-            if dbfile == None:
-                return jsonify({"ReturnCode": 0, "ErrorCode":"Missing database files, message a system admin"}), 505
-
-            conn = sqlite3.connect(f'SqliteStorage/{Username}/{dbfile}')
-            c = conn.cursor()
-            c.execute("SELECT name FROM sqlite_master WHERE type='table';")
-            response = c.fetchall()
-            try:
-                c.execute(Payload)
-                response = c.fetchall()
-
-                if response == []:
-                    response = None
-
-                return jsonify({"ReturnCode": 1, "Response":response}), 200
-            except sqlite3.OperationalError as a:
-                return jsonify({"ReturnCode": 0, "ErrorCode": str(a)}), 200
-
-#-------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     hostname = json.load(open("config.json"))["hostname"]
     port = json.load(open("config.json"))["port"]
     debug = json.load(open("config.json"))["debug"]
-    app.run(host=hostname, port=port, debug=debug)
+    if debug:
+        app.run(host=hostname, port=port, debug=True)
+    else:
+        serve(app, host=hostname, port=port)
