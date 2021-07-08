927097c44c67711c41a9792919680a045c3c9bed
Reece Harris <reece.harris98@protonmail.com>
2021-07-08 21:25:33 +0100

    Update and rename test.bat to AutoWindowsInstall.bat

---
+++ AutoWindowsInstall.bat
@@ -0,0 +1,6 @@
+git clone https://github.com/NotReeceHarris/DataStore3.git
+cd DataStore3/server
+pip install -r requirements.txt --no-warn-script-location
+cls
+python CreateUser.py
+python main.py
